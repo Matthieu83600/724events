5d12eaef22120308f665855f45f9e35009bec7b6
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2023-09-21 17:09:18 +0200

    Correction bug Filtres lors de la sélection d'une catégorie

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -16,7 +16,8 @@ const Select = ({
   const [value, setValue] = useState();
   const [collapsed, setCollapsed] = useState(true);
   const changeValue = (newValue) => {
-    onChange();
+    // Changement de onChange en ajoutant newValue
+    onChange(newValue);
     setValue(newValue);
     setCollapsed(newValue);
   };
--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -15,8 +15,10 @@ const EventList = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const filteredEvents = (
     (!type
+      // Affiche tous les événements
       ? data?.events
-      : data?.events) || []
+      // Permet de filtrer les événements en fonction de leur type
+      : data?.events.filter((event) => event.type === type)) || []
   ).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
