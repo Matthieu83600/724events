5c2a9093dc2d501ccf405bd8480467caba23318b
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2023-09-28 10:31:41 +0200

    Ajout des tests pour Date helper

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,15 @@
-/**
- * 
- */
+import { getMonth } from './index';
 
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
+            const date = new Date('2022-01-01')
+            expect(getMonth(date)).toEqual("janvier")
         });
+
         it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
+            const date = new Date('2022-07-08')
+            expect(getMonth(date)).toEqual("juillet")
         });
     });
 })
-
