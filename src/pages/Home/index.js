8460f80a1129ce264c94ba677d6dd27a59e894b7
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2023-10-05 14:32:48 +0200

    Changement du label de la dernière presta

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -129,7 +129,7 @@ const Page = () => {
           title={last?.title}
           date={new Date(last?.date)}
           small
-          label="Dernier Événement"
+          label={last?.type}
           data-testid="lastEvent"
         />
         )} 
